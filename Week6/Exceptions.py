b2dfbff6b10fbb53861c8ff8d8c3e1fc839c272d
trynalearn-code <michaelshloush@gmail.com>
2026-05-17 12:05:44 +0300

    adding week 6, with exceptions file

---
+++ Week6/Exceptions.py
@@ -0,0 +1,51 @@
+# #1.
+# def safe_int(s):
+#     try:
+#         return int(s)
+#     except ValueError:
+#         return None
+# print(safe_int("ab"))
+
+# #2.
+# def safe_divide(a,b):
+#     try:
+#         return a/b
+#     except ZeroDivisionError:
+#         return "undefined"
+
+# #3.
+# def read_first_line(path):
+#     try:
+#        with open(path, "r") as f:
+#            first_line = f.readline()
+#            return first_line
+#     except FileNotFoundError:
+#         return None
+    
+# #4.
+# def get_value(d,key):
+#     try:
+#         return d[key]
+#     except KeyError:
+#         return "missing"
+
+# #5.
+# def parse_ints(values):
+#     lst=[]
+#     for value in values:
+#         try:
+#             lst.append(int(value))
+#         except ValueError:
+#             continue
+#     return lst
+
+# print(parse_ints(["1", "2", "x", "3", "y"]))
+
+#6.
+def set_age(age):
+    if 150<age or age<0:
+        raise ValueError
+    else:
+        return age
+            
+print(set_age(490))

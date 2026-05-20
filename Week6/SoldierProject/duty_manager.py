84929f710db6f8a587a18a021e7736e7e26a9a79
trynalearn-code <michaelshloush@gmail.com>
2026-05-20 11:37:43 +0300

    adding add_duty_to_soldiers

--- Week6/SoldierProject/duty_manager.py
+++ Week6/SoldierProject/duty_manager.py
@@ -3,32 +3,42 @@
 # duty_manager.py
 # Responsibility: Business logic for duty management
 # ============================================================================
-
+import data as dt
 def add_duty_to_soldier(soldier_id: int, duty_name: str, day: str) -> None:
     """
     Adds a new duty to a soldier.
-    
     Type: Business Logic
-    
     Receives:
         soldier_id (int): Soldier's ID number
         duty_name (str): Duty name
         day (str): Day of the week (sunday/monday/tuesday/wednesday/thursday)
-    
     Returns:
         None - the function adds the duty or raises an exception
-    
     Raises:
         KeyError: If a soldier with this ID is not found in the system
         ValueError: If a duty with this name already exists for the soldier
         ValueError: If the day is invalid (friday/saturday or invalid value)
-    
     Why this function exists:
     Business logic for adding a duty.
     Performs checks and adds the duty to the soldier.
     Raises exceptions in case of errors instead of returning False.
     """
-    pass
+    if day.lower()=="friday" or day.lower()=="saturday":
+        raise ValueError("Aint gonna work on Saturdays")
+    for soldier in dt.soldiers:
+        if soldier["id"]==soldier_id:
+            for duty in soldier["duties"]:
+                if duty["name"]==duty_name:
+                    raise ValueError("A duty with this name already exists")
+            new_duty={
+                "name": duty_name,
+                "day": day,
+                "status" : "pending"
+            }
+            soldier["duties"].append(new_duty)
+            return
+    raise KeyError("Soldier ID not found")
+
 
 
 def update_duty_status(soldier_id: int, duty_name: str, new_status: str) -> None:

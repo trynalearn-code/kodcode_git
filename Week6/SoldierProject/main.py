52410dc84d2e938349f0c8faf0712bdfea644419
trynalearn-code <michaelshloush@gmail.com>
2026-05-24 11:04:53 +0300

    sending the last few edits on the main

--- Week6/SoldierProject/main.py
+++ Week6/SoldierProject/main.py
@@ -17,8 +17,7 @@ def get_user_choice() -> str:
 Takes user's choice
 Later can replace with GUI or website without changic logic
     """
-    return int(input("What action would you like to take?"))
-
+    return input("What action would you like to take?")
 
 def handle_add_soldier() -> None:
     """
@@ -60,9 +59,9 @@ def handle_view_soldiers() -> None:
         print("There aren't any soldiers in the program")
         return
     for soldier in soldiers:
-        print(f"ID: {soldier["id"]}")
-        print(f"Name: {soldier["name"]}")
-        print(f"Duties: {len(soldier["duties"])}")
+        print(f"ID: {soldier['id']}")
+        print(f"Name: {soldier['name']}")
+        print(f"Duties: {len(soldier['duties'])}")
 
 
 def handle_add_duty() -> None:
@@ -75,11 +74,15 @@ def handle_add_duty() -> None:
     Separation between UI and business logic.
     """
     try:
-        id=int(input("enter soldier ID")
+        id=int(input("enter soldier ID"))
         d_name=(input("Enter duty name: "))
         day=input("Enter day of duty")
-        
-               
+        add_duty_to_soldier(id, d_name, day)
+        print("success!")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except KeyError as e:
+        print(f"Error: {e}")
 
 
 def handle_update_duty_status() -> None:
@@ -93,8 +96,16 @@ def handle_update_duty_status() -> None:
     Why this function exists:
     Separation between UI and business logic.
     """
-    pass
-
+    try:
+        id=int(input("what is the soldier's ID?"))
+        name=input("what is the soldier's name?")
+        new_status= input("Enter new status: ")
+        update_duty_status(id, name, new_status)
+        print("Duty status updated")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except KeyError as e:
+        print(f"Error: {e}")
 
 def handle_view_soldier_duties() -> None:
     """
@@ -107,7 +118,20 @@ def handle_view_soldier_duties() -> None:
     Why this function exists:
     Separation between UI and business logic.
     """
-    pass
+    try:
+        id=int(input("Enter soldier id"))
+        duties=get_soldier_duties(id)
+        if not duties:
+            print("This soldier doesn't have no duties")
+            return
+        for duty in duties:
+            print(f"Name: {duty['name']}")
+            print(f"Day:{duty['day']}")
+            print(f"status: {duty['status']}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except KeyError as e:
+        print(f"Error: {e}")
 
 
 def main() -> None:
@@ -121,4 +145,26 @@ def main() -> None:
     Why this function exists:
     Entry point of the program. Manages the main flow.
     """
-    pass
+    while True:
+        show_menu()
+        action=get_user_choice()
+        if action== "1":
+            handle_add_soldier()
+        elif action=="2":
+            handle_remove_soldier()
+        elif action =="3":
+            handle_view_soldiers()
+        elif action == "4":
+            handle_add_duty()
+        elif action == "5":
+            handle_update_duty_status()
+        elif action == "6":
+            handle_view_soldier_duties()
+        elif action == "7":
+            print("bye")
+            break
+        else:
+            print("invalid input")
+
+if __name__== "__main__":
+    main()

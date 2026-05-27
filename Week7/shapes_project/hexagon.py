8da16d4edbf21363a6af9908ab8dcb0b6cca1dfb
trynalearn-code <michaelshloush@gmail.com>
2026-05-27 11:43:21 +0300

    sending in hexagon class

---
+++ Week7/shapes_project/hexagon.py
@@ -0,0 +1,12 @@
+from calculator import Shape
+from math import sqrt
+
+class Hexagon(Shape):
+    def __init__(self, side):
+        self.side=side
+    def get_area(self):
+        return (3*sqrt(3)*self.side**2)/2
+    def get_perimeter(self):
+        return self.side*6
+    def __str__(self):
+        return f"Hexagon. length of side:{self.side}"

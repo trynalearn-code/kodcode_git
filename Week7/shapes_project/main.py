fe9a6cf2a262c0e0307d358704ed2d42fdc1c4b1
trynalearn-code <michaelshloush@gmail.com>
2026-05-27 12:08:01 +0300

    finished up the main class

--- Week7/shapes_project/main.py
+++ Week7/shapes_project/main.py
@@ -1,16 +1,21 @@
 from rectangle import Rectangle
 from square import Square
-
+from triangle import Triangle
+from circle import Circle
+from hexagon import Hexagon
 rect = Rectangle(5, 10)
 
-print(rect)
-print("Area:", rect.get_area())
-print("Perimeter:", rect.get_perimeter())
+rect = Rectangle(5, 10)
+sq = Square(4)
+tri = Triangle(10, 5, 6, 8)
+circle = Circle(7)
+hexagon = Hexagon(3)
 
+shapes = [rect, sq, tri, circle, hexagon]
 
+for shape in shapes:
+    print("Shape:", shape.__class__.__name__)
+    print("Area:", shape.get_area())
+    print("Perimeter:", shape.get_perimeter())
+    print()
 
-square = Square(5)
-
-print(square)
-print("Area:", square.get_area())
-print("Perimeter:", square.get_perimeter())

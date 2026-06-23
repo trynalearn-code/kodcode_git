90eb5164abd6e02b828cf62c3fe90d033187aff7
trynalearn-code <michaelshloush@gmail.com>
2026-06-23 16:10:48 +0300

    sending in strings

---
+++ Week11/strings.js
@@ -0,0 +1,78 @@
+// //1.
+// const words = " hello world "
+// const trimmed = words.trim()
+// console.log(trimmed)
+
+// //2.
+// const myString = "user@gmail.com"
+// console.log(myString.includes("@"))
+
+// //3.
+// const myString = "javascript"
+// console.log(myString.toUpperCase())
+
+// //4.
+// const myString = "HELLO WORLD"
+// console.log(myString.toLowerCase())
+
+// //6.
+// const myString = "JavaScript"
+// console.log(myString.slice(0,4))
+
+// //7.
+// const myString = "JavaScript"
+// console.log(myString.slice(4))
+
+// //8.
+// const myString = "https://example.com"
+// console.log(myString.startsWith("https"))
+
+
+// //9.
+// const myString = "https://example.com"
+// console.log(myString.endsWith(".com"))
+
+// //10.
+// const myString = "hello world"
+// console.log(myString.replace("hello", "hi"))
+
+// //11.
+// const myString = "banana"
+// console.log(myString.replaceAll("a", "o"))
+
+// //12.
+// const myString = "one two three"
+// console.log(myString.split(" "))
+
+// //13.
+// const myString = "apple,banana,grape"
+// console.log(myString.split(",").slice(0,2))
+
+// //14.
+// const myString = "banana"
+// console.log(myString.indexOf("a"))
+
+// //15.
+// const myString = "banana"
+// console.log(myString.lastIndexOf("a"))
+
+// //16.
+// const myString = "7"
+// console.log(myString.padStart(3,"0"))
+
+// //19.
+// const myString = "Hello"
+// console.log(myString.charAt(0))
+
+// //20.
+// const myString = "Hello"
+// console.log(myString.at(-1))
+
+// //21.
+// const str1 = "hello"
+// const str2 = "world"
+// console.log(str1.concat(" ",str2))
+
+//22.
+const str1 = "JavaScript"
+console.log(str1.substring(2,6))

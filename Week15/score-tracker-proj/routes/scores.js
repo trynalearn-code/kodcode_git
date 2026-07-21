be096afe80826ee3c04e8f73400db4630f37a4dd
trynalearn-code <michaelshloush@gmail.com>
2026-07-21 16:15:51 +0300

    sending in get route

--- Week15/score-tracker-proj/routes/scores.js
+++ Week15/score-tracker-proj/routes/scores.js
@@ -1,13 +1,39 @@
 import { Router } from "express";
-
+import db from "../db.js";
 const router = Router()
 
-router.post("/scores", (req, res)=>{
-    console.log(req.body)
-    res.status(201).json({
-        success: true,
-        message: "created score"
-    })
+router.post("/scores", async (req, res)=>{
+    try {
+        const scores = db.collection("scores")
+        await scores.insertOne(req.body)
+        res.status(201).json({
+            success: true,
+            message: "created score"
+        })
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:"Error"
+        })
+    }
+})
+
+router.get("/scores", async (req, res)=>{
+    try {
+        const scores = db.collection("scores")
+        const allScores = await scores.find().toArray()
+        res.status(200).json({
+            success:true,
+            message:allScores
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Error"
+        })
+    }
 })
 
 export default router
